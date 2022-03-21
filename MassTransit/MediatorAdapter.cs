e13838a134d92246d06bb0aaa212c75b5a82e1e3
Aleksandr Gorodnichuk <a.gorodnichuk@linker.by>
2022-03-21 16:56:38 +0300

    Рабочий код

--- MassTransit/MediatorAdapter.cs
+++ MassTransit/MediatorAdapter.cs
@@ -1,13 +1,16 @@
 ﻿using MassTransit;
 using MassTransit.Mediator;
+using MassTransitShared.ForGetConsumers;
+using System.Reflection;
 
 namespace MassTransitShared
 {
     public static class MediatorAdapter
     {
-        public static async Task<TResponse> send<TRequest, TResponse>(this IMediator mediator, TRequest request, IBus bus = null) where TRequest : class, IRequest<TResponse> where TResponse: class
+        public static async Task<TResponse> send<TRequest, TResponse>(this IMediator mediator, TRequest request, Assembly assembly, IBus bus = null) where TRequest : class, IRequest<TResponse> where TResponse: class
         //public static async Task<TResponse> send<TResponse>(this IMediator mediator, IRequest<TResponse> request, IBus bus = null) where TResponse : class
         {
+            //var consumers = TypeExt.GetConsumers(null, assembly);
             try
             {
                 var client = mediator.CreateRequest(request);
--- Query/Controllers/ViewController.cs
+++ Query/Controllers/ViewController.cs
@@ -3,6 +3,7 @@ using MassTransit.Mediator;
 using MassTransitShared;
 using MediatorTest1.Consumers;
 using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
 
 namespace Query.Controllers
 {
@@ -20,12 +21,12 @@ namespace Query.Controllers
         [HttpGet("GetView")]
         public async Task<IActionResult> GetView()
         {
-            return Ok(await _mediator.send<GetAllRulesQuery, string2>(new GetAllRulesQuery() { number = "7" }, _bus));
+            return Ok(await _mediator.send<GetAllRulesQuery, string2>(new GetAllRulesQuery() { number = "7" }, Assembly.GetExecutingAssembly(), _bus));
         }
         [HttpGet("GetViewLocal")]
         public async Task<IActionResult> GetViewLocal()
         {
-            return Ok(await _mediator.send<GetQuery, string3>(new GetQuery() { number = "5" }, _bus));
+            return Ok(await _mediator.send<GetQuery, string3>(new GetQuery() { number = "5" }, Assembly.GetExecutingAssembly(), _bus));
         }
     }
 }
